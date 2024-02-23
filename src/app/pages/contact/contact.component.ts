d1291662b7932c0c5bb3a8bb293d4825432a518c
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-02-24 02:44:44 +0400

    added awaring text

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -10,15 +10,22 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  showText: boolean = false;
   reactiveForm: FormGroup = new FormGroup({
     full_name: new FormControl('Name Surname', [Validators.required]),
     email: new FormControl('________@___.__', [Validators.required, Validators.email]),
     message: new FormControl('Enter your message', [Validators.required])
   })
   onFormSubmitted(){
+    
     console.log(this.reactiveForm);
     console.log(this.reactiveForm.valid);
     
+  }
+  showTextFunc(){
+    if(!this.reactiveForm.valid){
+      this.showText = !this.showText
+    }
   }
 
 }
