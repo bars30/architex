c064bb68a3ac8d50a27196a0dddd35787fdcab65
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-02-24 01:53:06 +0400

    added responsive css to projects page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css', './projects.component-res.css']
 })
 export class ProjectsComponent {
 
